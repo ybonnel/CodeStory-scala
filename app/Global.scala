6c2254bdd706a9c6c6b0bfdfb484aa0d2cd8e134
ybonnel <ybonnel@gmail.com>
2013-01-30 10:44:03 +0100

    Add logs

--- app/Global.scala
+++ app/Global.scala
@@ -2,11 +2,22 @@ import play.api.Logger
 import play.api.mvc.Handler
 import play.GlobalSettings
 import play.mvc.Http.RequestHeader
+import play.mvc.Result
 
 
 class Global extends GlobalSettings {
   override def onRouteRequest(request: RequestHeader): Handler = {
-    Logger.info(request.toString);
+    Logger.info(request.toString)
     super.onRouteRequest(request)
+  }
+
+  override def onError(request: RequestHeader, t: Throwable): Result = {
+    Logger.error("Error", t)
+    super.onError(request, t)
+  }
+
+  override def onBadRequest(request: RequestHeader, error: String): Result = {
+    Logger.warn("Bad request : " + error)
+    super.onBadRequest(request, error)
   }
 }
